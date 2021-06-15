b42cf1ddb10bf2bf65c3d8560d855f3e1f121f38
marquemorquemer <m.solssilva@uandresbello.edu>
2021-06-15 02:19:31 -0400

    mov jp, enemy mov, change selector

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public int damage = 1;
+    public Vector2 direction;
+    public float speed = 10;
+    public string target;
+    public float timeToDestroy = 3;
+
+    private void Start()
+    {
+        Destroy(gameObject, timeToDestroy);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.CompareTag(target))
+        {
+            Health health = other.GetComponent<Health>();
+            if (health == null)
+                return;
+            health.TakeDamage(damage);
+            Destroy(gameObject);
+        }
+
+    }
+    private void Update()
+    {
+        transform.Translate(direction * speed * Time.deltaTime);
+        
+    }
+    public void DirectionBullet(Vector2 dir)
+    {
+        direction = dir;
+    }
+}
---
+++ Assets/Scripts/Shoot.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour
+{
+    public GameObject bullet;
+    public float timeToShoot = 3;
+    public bool isPlayer;
+
+    private void Start()
+    {
+        if(!isPlayer)
+        {
+            StartCoroutine(ShootingEnemy());
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(isPlayer)
+        {
+            if (Input.GetButtonDown("Jump") || Input.GetButtonDown("Fire1"))
+            {
+                ShootBullet(GetComponent<Movement>().directionShoot);
+            }
+        }        
+    }
+    void ShootBullet(Vector2 dir)
+    {
+        GameObject bulletGO = Instantiate(bullet, transform.position, Quaternion.identity);
+        bulletGO.GetComponent<Bullet>().DirectionBullet(dir);
+        if(isPlayer)
+        {
+            bulletGO.GetComponent<SpriteRenderer>().color = Color.green;
+            bulletGO.GetComponent<Bullet>().target = "Enemy";
+        }
+        else
+        {
+            bulletGO.GetComponent<SpriteRenderer>().color = Color.red;
+            bulletGO.GetComponent<Bullet>().target = "Player";
+        }
+    }
+    IEnumerator ShootingEnemy()
+    {
+        while(true)
+        {
+            yield return new WaitForSeconds(timeToShoot);
+            ShootBullet(GetComponent<FollowTarget>().directionFollow);
+        }
+    }
+}
